68e32385522c7f35f70fcab1dee03fe559f3b63a
apna-collegeLearn <adityamallick002@gmail.com>
2026-04-10 20:08:26 +0530

    Add solution: Biggest Single Number

---
+++ 28-biggest-single-number/solution.sql
@@ -0,0 +1,11 @@
+-- Problem: Biggest Single Number
+-- Link: https://leetcode.com/problems/biggest-single-number/
+
+SELECT 
+    MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+) t;
