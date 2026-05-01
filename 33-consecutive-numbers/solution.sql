9640fcfe70c2ddf81f7a693094b2b21e37a51598
apna-collegeLearn <adityamallick002@gmail.com>
2026-05-01 10:03:25 +0530

    Add solution: Consecutive Numbers

---
+++ 33-consecutive-numbers/solution.sql
@@ -0,0 +1,13 @@
+-- Problem: Consecutive Numbers
+-- Link: https://leetcode.com/problems/consecutive-numbers/
+
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT 
+        num,
+        LEAD(num, 1) OVER (ORDER BY id) AS next1,
+        LEAD(num, 2) OVER (ORDER BY id) AS next2
+    FROM Logs
+) t
+WHERE num = next1 
+  AND next1 = next2;
