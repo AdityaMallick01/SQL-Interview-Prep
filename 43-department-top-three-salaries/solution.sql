a92325b8d16f4f0be430d898c5d20c555b2f83d7
apna-collegeLearn <adityamallick002@gmail.com>
2026-05-10 20:16:58 +0530

    Add solution: Department Top Three Salaries

---
+++ 43-department-top-three-salaries/solution.sql
@@ -0,0 +1,19 @@
+-- Problem: Department Top Three Salaries
+-- Link: https://leetcode.com/problems/department-top-three-salaries/
+
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM (
+    SELECT
+        *,
+        DENSE_RANK() OVER(
+            PARTITION BY departmentId
+            ORDER BY salary DESC
+        ) AS rnk
+    FROM Employee
+) e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE rnk <= 3;
